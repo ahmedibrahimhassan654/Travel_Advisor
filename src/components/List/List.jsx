4cd055f037f76a78a5ec67ec4029e3b1d5917543
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-01-19 01:51:16 +0200

    create and style list component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,43 +8,63 @@ import {
   FormControl,
   Select,
 } from "@material-ui/core";
-
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
 import useStyles from "./style";
 
 const List = () => {
   const classes = useStyles();
   const [type, setType] = useState("resturants");
-  
+  const [rating, setRating] = useState("");
+  const places = [
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best stack" },
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best stack" },
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best stack" },
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best stack" },
+  ];
   return (
     <div className={classes.container}>
       <Typography variant="h4">Food & Drink around you</Typography>
       <FormControl className={classes.formControl}>
-        <InputLabel>type</InputLabel>
+        <InputLabel id="type">Type</InputLabel>
         <Select
-          vlaue={type}
-          onChange={(e) => {
-            setType(e.target.value);
-          }}
+          id="type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
         >
-          <MenuItem vlaue="resturants">Resturants</MenuItem>
-          <MenuItem vlaue="hotels">Hotels</MenuItem>
-          <MenuItem value="attractions">Attraction</MenuItem>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
         </Select>
       </FormControl>
 
       <FormControl className={classes.formControl}>
-        <InputLabel>Rating</InputLabel>
+        <InputLabel id="rating">Rating</InputLabel>
         <Select
-          vlaue={type}
-          onChange={(e) => {
-            setType(e.target.value);
-          }}
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
         >
-          <MenuItem vlaue="resturants">Resturants</MenuItem>
-          <MenuItem vlaue="hotels">Hotels</MenuItem>
-          <MenuItem value="attractions">Attraction</MenuItem>
+          <MenuItem value="">All</MenuItem>
+          <MenuItem value="3">Above 3.0</MenuItem>
+          <MenuItem value="4">Above 4.0</MenuItem>
+          <MenuItem value="4.5">Above 4.5</MenuItem>
         </Select>
       </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
