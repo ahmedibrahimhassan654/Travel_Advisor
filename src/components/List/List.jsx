f4de4f5e523f87984687ba701cddbecefbdbdd8f
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-01-19 00:28:57 +0200

    finished map component and start working in list component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import useStyles from "./style";
 
 const List = () => {
-    return (
-        <div>
-                <h1>List</h1>
-        </div>
-    )
-}
+  const classes = useStyles();
+  const [type, setType] = useState("resturants");
+  
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Drink around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>type</InputLabel>
+        <Select
+          vlaue={type}
+          onChange={(e) => {
+            setType(e.target.value);
+          }}
+        >
+          <MenuItem vlaue="resturants">Resturants</MenuItem>
+          <MenuItem vlaue="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attraction</MenuItem>
+        </Select>
+      </FormControl>
 
-export default List
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select
+          vlaue={type}
+          onChange={(e) => {
+            setType(e.target.value);
+          }}
+        >
+          <MenuItem vlaue="resturants">Resturants</MenuItem>
+          <MenuItem vlaue="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attraction</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+export default List;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
 
+import useStyles from "./style.js";
 const Map = () => {
+  const matches = useMediaQuery("(min-width:600px)");
+  const classes = useStyles();
+
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <div>
-      <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ keys: "AIzaSyBUKT-i-j3sXj_Fh8HblpWhHceACDKA8g4" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
     </div>
   );
 };
